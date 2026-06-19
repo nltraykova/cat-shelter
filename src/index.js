d9f74b3586220050026ffe321c72e375d74ff625
Nadezhda Traykova <n.l.traykova@gmail.com>
2026-06-19 20:11:24 +0300

    Add http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import http from 'http';
+
+//request listener
+const server = http.createServer((req, res) => {
+    res.write('Hello World!');
+    res.end();
+});
+
+server.listen(5000, () => console.log('Server running on htttp://localhost:5000...'));
